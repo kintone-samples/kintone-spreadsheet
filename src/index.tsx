b280794a95ed598cfbac6599e4eaaab56cab36c0
Kazuki Murahama <kazuki.murahama@gmail.com>
2019-11-04 12:27:25 +0900

    WIP implementing base part

---
+++ src/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HotTable } from "@handsontable/react";
+import "handsontable/dist/handsontable.full.css";
+import { getColumnData } from "./js/utils/utils";
+
+(PLUGIN_ID => {
+  kintone.events.on("app.record.index.show", event => {
+    // 試しに列は固定
+    const columns = ["text"];
+
+    (async () => {
+      const { records } = await kintone.api(
+        kintone.api.url("/k/v1/records", true),
+        "GET",
+        { app: kintone.app.getId() }
+      );
+      const { colHeaders, columnDatas, dataSchema } = await getColumnData(
+        columns
+      );
+      ReactDOM.render(
+        <HotTable
+          data={records}
+          rowHeaders={true}
+          width="600"
+          height="300"
+          colHeaders={colHeaders}
+          columns={columnDatas}
+          dataSchema={dataSchema}
+        />,
+        document.getElementById("app")
+      );
+    })();
+  });
+})(kintone.$PLUGIN_ID);
---
+++ src/js/utils/utils.ts
@@ -0,0 +1,85 @@
+import Handsonlable from "handsontable";
+const ALLOWED_FIELD_TYPES = [
+  "RECORD_NUMBER",
+  "CREATED_TIME",
+  "UPDATED_TIME",
+  "CREATOR",
+  "MODIFIER",
+  "STATUS",
+  "STATUS_ASSIGNEE"
+];
+const ARRAY_FIELDS = [
+  "CHECK_BOX",
+  "MULTI_SELECT",
+  "FILE",
+  "USER_SELECT",
+  "CATEGORY",
+  "SUBTABLE",
+  "ORGANIZATION_SELECT",
+  "GROUP_SELECT"
+];
+const NOT_ALLOWED_EDIT_FIELDS = [
+  "USER_SELECT",
+  "CALC",
+  "CHECK_BOX",
+  "MULTI_SELECT",
+  "FILE",
+  "CATEGORY",
+  "SUBTABLE",
+  "ORGANIZATION_SELECT",
+  "GROUP_SELECT"
+];
+
+export const getColumnData = async (columns: string[]) => {
+  const resp = await kintone.api("/k/v1/app/form/fields", "GET", {
+    app: kintone.app.getId()
+  });
+  // ヘッダーの取得
+  const colHeaders = columns.map(column => {
+    return resp.properties[column].label;
+  });
+
+  // 各セルの設定
+  const columnDatas: Handsonlable.ColumnSettings[] = columns.map(column => {
+    const columnData: Handsonlable.ColumnSettings = { data: `${column}.value` };
+
+    // if type is DROP_DOWN, add type and source property
+    if (
+      resp.properties[column].type === "DROP_DOWN" ||
+      resp.properties[column].type === "RADIO_BUTTON"
+    ) {
+      columnData.type = "dropdown";
+      columnData.source = Object.keys(resp.properties[column].options);
+    }
+
+    // if (resp.properties[column].type === "USER_SELECT") {
+    //   columnData.renderer = utils.userSelectRenderer;
+    // }
+
+    // if (resp.properties[column].type === "CHECK_BOX") {
+    //   columnData.renderer = utils.checkboxRenderer;
+    // }
+
+    // set read only
+    if (
+      ALLOWED_FIELD_TYPES.concat(NOT_ALLOWED_EDIT_FIELDS).indexOf(
+        resp.properties[column].type
+      ) !== -1
+    ) {
+      columnData.readOnly = true;
+    }
+    return columnData;
+  });
+
+  // データスキーマの作成
+  const dataSchema: Handsonlable.RowObject = {};
+  // TODO: reduceつかう
+  columns.forEach(column => {
+    dataSchema[column] = {
+      type: resp.properties[column].type,
+      value: resp.properties[column].defaultValue
+    };
+  });
+
+  return { colHeaders, columnDatas, dataSchema };
+};
