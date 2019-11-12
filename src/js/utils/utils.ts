e6782fdf901ba539901d3dde6cefcc90b3a1c469
Kazuki Murahama <kazuki.murahama@gmail.com>
2019-11-13 00:29:42 +0900

    Added lint

--- src/js/utils/utils.ts
+++ src/js/utils/utils.ts
@@ -1,71 +1,64 @@
-import Handsonlable from "handsontable";
+import Handsonlable from 'handsontable';
 const ALLOWED_FIELD_TYPES = [
-  "RECORD_NUMBER",
-  "CREATED_TIME",
-  "UPDATED_TIME",
-  "CREATOR",
-  "MODIFIER",
-  "STATUS",
-  "STATUS_ASSIGNEE"
+  'RECORD_NUMBER',
+  'CREATED_TIME',
+  'UPDATED_TIME',
+  'CREATOR',
+  'MODIFIER',
+  'STATUS',
+  'STATUS_ASSIGNEE',
 ];
 const ARRAY_FIELDS = [
-  "CHECK_BOX",
-  "MULTI_SELECT",
-  "FILE",
-  "USER_SELECT",
-  "CATEGORY",
-  "SUBTABLE",
-  "ORGANIZATION_SELECT",
-  "GROUP_SELECT"
+  'CHECK_BOX',
+  'MULTI_SELECT',
+  'FILE',
+  'USER_SELECT',
+  'CATEGORY',
+  'SUBTABLE',
+  'ORGANIZATION_SELECT',
+  'GROUP_SELECT',
 ];
 const NOT_ALLOWED_EDIT_FIELDS = [
-  "USER_SELECT",
-  "CALC",
-  "CHECK_BOX",
-  "MULTI_SELECT",
-  "FILE",
-  "CATEGORY",
-  "SUBTABLE",
-  "ORGANIZATION_SELECT",
-  "GROUP_SELECT"
+  'USER_SELECT',
+  'CALC',
+  'CHECK_BOX',
+  'MULTI_SELECT',
+  'FILE',
+  'CATEGORY',
+  'SUBTABLE',
+  'ORGANIZATION_SELECT',
+  'GROUP_SELECT',
 ];
 
 export const getColumnData = async (columns: string[]) => {
-  const resp = await kintone.api("/k/v1/app/form/fields", "GET", {
-    app: kintone.app.getId()
+  const resp = await kintone.api('/k/v1/app/form/fields', 'GET', {
+    app: kintone.app.getId(),
   });
   // ヘッダーの取得
-  const colHeaders = columns.map(column => {
+  const colHeaders = columns.map((column) => {
     return resp.properties[column].label;
   });
 
   // 各セルの設定
-  const columnDatas: Handsonlable.ColumnSettings[] = columns.map(column => {
+  const columnDatas: Handsonlable.ColumnSettings[] = columns.map((column) => {
     const columnData: Handsonlable.ColumnSettings = { data: `${column}.value` };
 
     // if type is DROP_DOWN, add type and source property
-    if (
-      resp.properties[column].type === "DROP_DOWN" ||
-      resp.properties[column].type === "RADIO_BUTTON"
-    ) {
-      columnData.type = "dropdown";
+    if (resp.properties[column].type === 'DROP_DOWN' || resp.properties[column].type === 'RADIO_BUTTON') {
+      columnData.type = 'dropdown';
       columnData.source = Object.keys(resp.properties[column].options);
     }
 
-    if (resp.properties[column].type === "USER_SELECT") {
+    if (resp.properties[column].type === 'USER_SELECT') {
       columnData.renderer = userSelectRenderer;
     }
 
-    if (resp.properties[column].type === "CHECK_BOX") {
+    if (resp.properties[column].type === 'CHECK_BOX') {
       columnData.renderer = checkboxRenderer;
     }
 
     // set read only
-    if (
-      ALLOWED_FIELD_TYPES.concat(NOT_ALLOWED_EDIT_FIELDS).indexOf(
-        resp.properties[column].type
-      ) !== -1
-    ) {
+    if (ALLOWED_FIELD_TYPES.concat(NOT_ALLOWED_EDIT_FIELDS).indexOf(resp.properties[column].type) !== -1) {
       columnData.readOnly = true;
     }
     return columnData;
@@ -74,10 +67,10 @@ export const getColumnData = async (columns: string[]) => {
   // データスキーマの作成
   const dataSchema: Handsonlable.RowObject = {};
   // TODO: reduceつかう
-  columns.forEach(column => {
+  columns.forEach((column) => {
     dataSchema[column] = {
       type: resp.properties[column].type,
-      value: resp.properties[column].defaultValue
+      value: resp.properties[column].defaultValue,
     };
   });
 
@@ -91,25 +84,17 @@ const userSelectRenderer: Handsonlable.renderers.BaseRenderer = (
   col,
   prop,
   value,
-  cellProperties
+  cellProperties,
 ) => {
   if (!value) return td;
-  td.innerText = value.map(v => v.name).join(", ");
-  td.style.color = "#777";
+  td.innerText = value.map((v) => v.name).join(', ');
+  td.style.color = '#777';
   return td;
 };
 
-const checkboxRenderer: Handsonlable.renderers.Checkbox = (
-  instance,
-  td,
-  row,
-  col,
-  prop,
-  value,
-  cellProperties
-) => {
+const checkboxRenderer: Handsonlable.renderers.Checkbox = (instance, td, row, col, prop, value, cellProperties) => {
   if (!value.length) return td;
-  td.innerText = value.join(", ");
-  td.style.color = "#777";
+  td.innerText = value.join(', ');
+  td.style.color = '#777';
   return td;
 };
