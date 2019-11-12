0765bd47ecbfd918d1b1ea9fca4b0bad028b659d
Kazuki Murahama <kazuki.murahama@gmail.com>
2019-11-13 00:50:55 +0900

    [wip] added config file

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,21 @@
-import path from "path";
-import { Configuration } from "webpack";
-import TerserPlugin from "terser-webpack-plugin";
+import path from 'path';
+import { Configuration } from 'webpack';
+import TerserPlugin from 'terser-webpack-plugin';
 
-const DEBUG = process.env.NODE_ENV !== "production";
+const DEBUG = process.env.NODE_ENV !== 'production';
 
 const config: Configuration = {
-  mode: DEBUG ? "development" : "production",
-  devtool: DEBUG ? "cheap-module-eval-source-map" : undefined,
+  mode: DEBUG ? 'development' : 'production',
+  devtool: DEBUG ? 'cheap-module-eval-source-map' : undefined,
+  entry: {
+    main: './src/index.tsx',
+    config: './src/js/config',
+  },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
     alias: {
-      "~/src": path.resolve(__dirname, "./src")
-    }
+      '~/src': path.resolve(__dirname, './src'),
+    },
   },
   plugins: [],
   optimization: {
@@ -22,52 +26,52 @@ const config: Configuration = {
             warnings: false,
             unused: true,
             dead_code: true,
-            drop_console: true
+            drop_console: true,
           },
-          output: { comments: false }
-        }
-      })
+          output: { comments: false },
+        },
+      }),
     ],
     splitChunks: {
       cacheGroups: {
         commons: {
-          chunks: "initial",
+          chunks: 'initial',
           test: /[\\/]node_modules[\\/]/,
-          name: "vendor"
-        }
-      }
-    }
+          name: 'vendor',
+        },
+      },
+    },
   },
   output: {
-    path: path.resolve(__dirname, "src/dist"),
-    filename: DEBUG ? "[name].bundle.js" : "[name].[chunkhash:8].js",
-    publicPath: "/"
+    path: path.resolve(__dirname, 'src/dist'),
+    filename: DEBUG ? '[name].bundle.js' : '[name].[chunkhash:8].js',
+    publicPath: '/',
   },
   module: {
     strictExportPresence: true,
     rules: [
       {
         test: /\.(png|jpg|gif)$/,
-        loader: "file-loader"
+        loader: 'file-loader',
       },
       {
         test: /\.[tj]sx?$/,
-        include: path.resolve(__dirname, "src"),
+        include: path.resolve(__dirname, 'src'),
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
             options: {
-              transpileOnly: true
-            }
-          }
-        ]
+              transpileOnly: true,
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      }
-    ]
-  }
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
 };
 
 export default config;
