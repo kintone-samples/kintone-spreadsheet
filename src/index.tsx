b8a9109062ec2b1ca3c5f2e1fee293960f5c3bce
Kazuki Murahama <kazuki.murahama@gmail.com>
2019-11-13 00:29:42 +0900

    Added lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HotTable } from "@handsontable/react";
-import "handsontable/dist/handsontable.full.css";
-import { getColumnData } from "./js/utils/utils";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HotTable } from '@handsontable/react';
+import 'handsontable/dist/handsontable.full.css';
+import { getColumnData } from './js/utils/utils';
 
 (() => {
-  kintone.events.on("app.record.index.show", event => {
+  kintone.events.on('app.record.index.show', (event) => {
     // 試しに列は固定
-    const columns = ["text", "checkbox", "user"];
+    const columns = ['text', 'checkbox', 'user'];
 
     (async () => {
-      const { records } = await kintone.api(
-        kintone.api.url("/k/v1/records", true),
-        "GET",
-        { app: kintone.app.getId() }
-      );
-      const { colHeaders, columnDatas, dataSchema } = await getColumnData(
-        columns
-      );
+      const { records } = await kintone.api(kintone.api.url('/k/v1/records', true), 'GET', {
+        app: kintone.app.getId(),
+      });
+      const { colHeaders, columnDatas, dataSchema } = await getColumnData(columns);
       ReactDOM.render(
         <HotTable
           data={records}
-          rowHeaders={true}
+          rowHeaders
           width="600"
           height="300"
           colHeaders={colHeaders}
           columns={columnDatas}
           dataSchema={dataSchema}
         />,
-        document.getElementById("app")
+        document.getElementById('app'),
       );
     })();
   });
